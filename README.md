5590ed63caad801c2467e44ad6ec1ba5fc4e624e
Niklas Kiefer <niklas.kiefer@camunda.com>
2019-05-21 16:38:43 +0200

    chore(project): initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# mocha-set-only
+
+Sets the [`only`](https://stackoverflow.com/questions/10832031/how-to-run-a-single-test-with-mocha) tag to a mocha-test for a given file
+
+```sh
+$ npm i mocha-set-only
+$ mocha-set-only -—f="/Users/niklas.kiefer/Github/bpmn-js/test/spec/ModelerSpec.js" -—test="should import simple process"
+```
+
+![gif](./resources/screencast.gif)
---
+++ index.js
@@ -0,0 +1,65 @@
+const fs = require('fs');
+
+const mri = require('mri');
+
+const {
+  help,
+  ...args
+} = mri(process.argv, {
+  alias: {
+    file: [ 'f' ],
+    help: [ 'h' ],
+    test: [ 't' ],
+  },
+  string: ['f', 't'],
+  default: {
+    help: false
+  }
+});
+
+
+if (help) {
+
+  printHelp();
+
+  process.exit(0);
+}
+
+run(args);
+
+async function run(options) {
+
+  let {
+    file,
+    test
+  } = options;
+
+  test = test[1] || '';
+
+  if (!file || !test) {
+
+    console.error('File and Test name have to be provided!');
+    printHelp();
+
+    process.exit(1);
+  }
+
+  const f = fs.readFileSync(file);
+
+  const contents = f.toString('utf8');
+
+  const updated = contents.replace(`('${test}`, `.only('${test}`);
+
+  fs.writeFileSync(file, updated);
+
+}
+
+function printHelp() {
+  console.log(`usage: node index.js [-f FILE_NAME] [-t TEST_NAME]
+    Sets a karma-mocha test to only.
+    Options:
+        -f, --file=FILE_NAME          test in which the file is located
+        -t, --test=TEST_NAME          test label
+        -h, --help                    print this help
+    `);
+}
